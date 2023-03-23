372f6166d6bd27e619c0d375f93e2a675ad58d2b
zhentao.zhong <zhentao.zhong@iglooinsure.com>
2023-03-23 15:12:18 +0800

    FEAT: 修改样式

--- src/layouts/index.tsx
+++ src/layouts/index.tsx
@@ -1,13 +1,21 @@
-import { Layout, Menu } from 'antd';
-import { useState } from 'react';
 import { Outlet, history, useIntl, SelectLang } from '@umijs/max';
+import { useState, useEffect } from 'react';
+
 import { TranslationOutlined } from '@ant-design/icons';
+import { Layout, Menu } from 'antd';
+
 import styles from './index.less';
 
 const { Header, Content } = Layout;
 export default () => {
   const { formatMessage } = useIntl();
   const [key, setKey] = useState(history.location.pathname);
+
+  useEffect(() => {
+    history.listen((h: any) => {
+      setKey(h.location.pathname);
+    });
+  });
 
   return (
     <Layout style={{ height: '100%' }}>
@@ -28,7 +36,6 @@ export default () => {
             ]}
             onClick={({ key }) => {
               history.push(`${key}`);
-              setKey(key);
             }}
             selectedKeys={[key]}
           />
@@ -44,7 +51,9 @@ export default () => {
         </div>
       </Header>
       <Content>
-        <Outlet />
+        <div className={styles.wrapper}>
+          <Outlet />
+        </div>
       </Content>
     </Layout>
   );
